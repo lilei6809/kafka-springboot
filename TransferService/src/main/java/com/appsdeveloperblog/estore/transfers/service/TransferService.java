ac76a1677bed71bb5f4faad5cd4145af19dd3cda
lilei6809 <lilei6809@gmail.com>
2024-11-12 11:31:35 +0000

    Revert "Add Deposit, Transfer and Withdrawal services"
    This reverts commit 134d97a4f7cd9f1a8d1621aff3a4a9579cb10696.

--- TransferService/src/main/java/com/appsdeveloperblog/estore/transfers/service/TransferService.java
+++
@@ -1,7 +0,0 @@
-package com.appsdeveloperblog.estore.transfers.service;
-
-import com.appsdeveloperblog.estore.transfers.model.TransferRestModel;
-
-public interface TransferService {
-    public boolean transfer(TransferRestModel productPaymentRestModel);
-}
